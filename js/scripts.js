c8fbb8cfec674eef7daeaf66466bf60681b62dc9
Chris Turina and Joseph Tomlinson <chris@>
2017-09-21 10:56:57 -0700

    Refactored branching statement into a loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,46 +1,56 @@
+var Numerals = ["M","CM","D","CD", "C", "LC", "L", "XL", "X", "IX", "V","IV", "I"];
+var Numbers = [1000,900,500,400,100,90,50,40,10,9,5,4,1];
+
 var makeRomanNumeral = function(input) {
   var output = "";
   while (input > 0) {
-    if (input >= 1000) {
-      input = input - 1000;
-      output = output + "M";
-    } else if (input >= 900) {
-      input = input - 900;
-      output = output + "CM";
-    } else if (input >= 500) {
-      input = input - 500;
-      output = output + "D";
-    } else if (input >= 400) {
-      input = input - 400;
-      output = output + "CD";
-    } else if ( input >= 100 ) {
-      input = input - 100;
-      output = output + "C"
-    }  else if (input >= 90) {
-      input = input - 90
-      output = output + "LC"
-    } else if ( input >= 50 ) {
-      input = input - 50;
-      output = output + "L"
-    } else if ( input >= 40) {
-      input = input - 40;
-      output = output + "XL";
-    } else if ( input >= 10 ) {
-      input = input - 10;
-      output = output + "X"
-    } else if (input >= 9) {
-      input = input - 9;
-      output = output + "IX";
-    } else if ( input >= 5 ) {
-      input = input - 5;
-      output = output + "V"
-    } else if (input >= 4) {
-      input = input - 4;
-      output = output + "IV";
-    } else {
-      input = input - 1;
-      output = output + "I"
+    for ( var i=0; i < 13; i++){
+      var currentNumb = Numbers[i];
+      if (input >= currentNumb) {
+        output = output + Numerals[i];
+        input = input - currentNumb;
+      }
     }
+    // if (input >= 1000) {
+    //   input = input - 1000;
+    //   output = output + "M";
+    // } else if (input >= 900) {
+    //   input = input - 900;
+    //   output = output + "CM";
+    // } else if (input >= 500) {
+    //   input = input - 500;
+    //   output = output + "D";
+    // } else if (input >= 400) {
+    //   input = input - 400;
+    //   output = output + "CD";
+    // } else if ( input >= 100 ) {
+    //   input = input - 100;
+    //   output = output + "C"
+    // }  else if (input >= 90) {
+    //   input = input - 90
+    //   output = output + "LC"
+    // } else if ( input >= 50 ) {
+    //   input = input - 50;
+    //   output = output + "L"
+    // } else if ( input >= 40) {
+    //   input = input - 40;
+    //   output = output + "XL";
+    // } else if ( input >= 10 ) {
+    //   input = input - 10;
+    //   output = output + "X"
+    // } else if (input >= 9) {
+    //   input = input - 9;
+    //   output = output + "IX";
+    // } else if ( input >= 5 ) {
+    //   input = input - 5;
+    //   output = output + "V"
+    // } else if (input >= 4) {
+    //   input = input - 4;
+    //   output = output + "IV";
+    // } else {
+    //   input = input - 1;
+    //   output = output + "I"
+    // }
   }
   return output
 }
